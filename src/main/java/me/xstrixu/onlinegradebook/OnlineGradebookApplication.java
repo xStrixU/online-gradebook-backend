3d678205e0dd85291d6737f6bbcd644632cbcf59
xStrixU <strixu155@gmail.com>
2022-06-21 08:53:09 +0200

    Initial commit

---
+++ src/main/java/me/xstrixu/onlinegradebook/OnlineGradebookApplication.java
@@ -0,0 +1,13 @@
+package me.xstrixu.onlinegradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGradebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGradebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/xstrixu/onlinegradebook/OnlineGradebookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xstrixu.onlinegradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGradebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
