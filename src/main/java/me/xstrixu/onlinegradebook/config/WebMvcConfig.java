02e54bf06469ef87b725f7af029b8a7e1a7db803
xStrixU <strixu155@gmail.com>
2022-06-21 09:04:50 +0200

    Configure CORS

---
+++ src/main/java/me/xstrixu/onlinegradebook/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package me.xstrixu.onlinegradebook.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true);
+    }
+}
